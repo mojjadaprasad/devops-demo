bd3262aa631da5ccf0418da30e9f1f81650ad230
Your Name <you@example.com>
2021-07-19 12:42:07 +0530

    first pipeline

---
+++ src/main/java/com/app/Demo2.java
@@ -0,0 +1,24 @@
+package com.app;
+
+public class Demo2 {
+
+	
+	int id;
+	String name;
+	String email;
+	float pnumber;
+	
+	public void student() {
+		
+	}
+	
+	public class Demo3 extends Demo2{
+		
+		
+		private void prasad(){
+		
+	}
+	
+	}
+	
+}
---
+++ src/main/java/com/app/PojoClassDemo.java
@@ -0,0 +1,38 @@
+package com.app;
+
+public class PojoClassDemo {
+	
+	
+	int id;
+	String name;
+	String email;
+	float pnumber;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public float getPnumber() {
+		return pnumber;
+	}
+	public void setPnumber(float pnumber) {
+		this.pnumber = pnumber;
+	}
+	
+	
+
+}
